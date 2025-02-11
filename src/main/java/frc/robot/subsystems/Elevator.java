5f22358d0e624cff99f720bf62c67017f47b41af
jryurgil <jryurgil@gmail.com>
2025-02-11 18:24:44 -0500

    set elevator motor to follow, pid control setup

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -7,9 +7,13 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.SparkMax;
 
 import frc.robot.Constants.ElevatorConstants;
@@ -18,8 +22,9 @@ import frc.robot.Constants.ElevatorConstants;
 public class Elevator extends SubsystemBase {
   private final SparkMax elevator1;
   private final SparkMax elevator2;
+  private final SparkMaxConfig elevator2config;
   private final RelativeEncoder elevatorEncoder;
-
+  private double target;
 
   private final SparkClosedLoopController elevatorClosedLoopController;
   //need to add spark max motor controler for final design
@@ -30,9 +35,14 @@ public class Elevator extends SubsystemBase {
     elevatorEncoder = elevator1.getEncoder();
   
     elevatorClosedLoopController = elevator1.getClosedLoopController();
-elevator2.configure(null, null, null);//set follower
+    elevator2config = new SparkMaxConfig();
+    elevator2config.follow(elevator1, true);
+    elevator2config.idleMode(IdleMode.kBrake);
+    elevator2config.closedLoop.pid(1,0,0);//PID values
+    elevator2.configure(elevator2config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);//set follower
     elevatorEncoder.setPosition(0);
-    elevatorClosedLoopController.setReference(0, ControlType.kPosition);
+    target = elevatorEncoder.getPosition();
+    elevatorClosedLoopController.setReference(target, ControlType.kPosition);
   }
 
 
@@ -40,7 +50,7 @@ elevator2.configure(null, null, null);//set follower
   @Override
   public void periodic() {
     SmartDashboard.putNumber("Elevator Position", elevatorEncoder.getPosition());
-    SmartDashboard.putNumber("Elevator Target", 0);
+    SmartDashboard.putNumber("Elevator Target", target);
   }
 
 }
