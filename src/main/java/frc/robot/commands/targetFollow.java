82dd9c0a783f15da70e0792a6b792325bfc97cbe
jryurgil <jryurgil@gmail.com>
2025-03-25 19:18:20 -0400

    tested dual score auto

--- src/main/java/frc/robot/commands/targetFollow.java
+++ src/main/java/frc/robot/commands/targetFollow.java
@@ -21,7 +21,14 @@ public class targetFollow extends Command {
   private final double LOAD_DISTANCE = 3; // distance in m to back up for load station from reef
   private final int LOAD_Y_OFFSET = 25;// lateral offset to back up to when loading
   private final int X_OFFSET = 17;//x offset to score on coral
-  private long timer;
+  private long timer;//step timer
+  private long autotimer;//overall auto timer
+  //constants controlling speed and time in auto
+  private final double MAX_FORWARD_SPD = 1.0;//max speed moving toward reef and load, tested value 0.15 (working), tested 0.3 (almost)
+  private final int FINAL_APPROACH_DWELL = 2000;//ms for final approach
+  private final int WAIT_AT_LOAD_DWELL = 2000;//ms for waiting at load
+  private final int MOVE_TO_LOAD_TIMEOUT = 3000;//ms for exiting move to load
+  private final int DRIVE_TO_REEF_TIMEOUT = 4000;//ms for exiting move to reef
 
   private final double ROT_DEADBAND = 0.01;
   private final double XSPEED_DEADBAND = 0.01;
@@ -37,14 +44,40 @@ public class targetFollow extends Command {
 
   private int piecesScored;//number of pieces scored
   private int pipeline;
+  private int position;
+
   /** Creates a new targetFollow. */
-  public targetFollow(DriveSubsystem driveSubsystem, RollerSubsystem rollerSubsystem, Elevator elevatorSubsystem, int startingpipeline) {
+  public targetFollow(DriveSubsystem driveSubsystem, RollerSubsystem rollerSubsystem, Elevator elevatorSubsystem, int startingposition) {
     // Use addRequirements() here to declare subsystem dependencies.
 
     this.driveSubsystem = driveSubsystem;
     this.rollerSubsystem = rollerSubsystem;
     this.elevatorSubsystem = elevatorSubsystem;
-    pipeline = startingpipeline;
+    this.position = startingposition;//tagfollower position 0 - left, 1 - center, 2 - right
+
+    /*
+     * Pipelines
+     * center tags 10,21
+     * inner corner tags 9,11,20,22
+     * all reef tags 6-11, 17-22
+     * loading tags 1,2,12,13
+     * 
+     * 0: center_tags
+     * 1: inner_corners
+     * 2: outer_corners (6,8,17,19)
+     * 3: not defined on LL
+     * 4: not defined on LL
+     */
+    //tagfollower position 0 - left, 1 - center, 2 - right
+    if(startingposition==1){
+      //center
+      pipeline = 0;
+
+    }else{
+      //left or right
+      pipeline = 1;
+    }
+    
 
     addRequirements(driveSubsystem);
     addRequirements(rollerSubsystem);
@@ -57,6 +90,7 @@ public class targetFollow extends Command {
     System.out.println("targetFollow cmd started");
     step = DRIVE_TO_REEF;// steps in the program, starting at this step
     timer = System.currentTimeMillis();
+    autotimer = System.currentTimeMillis();
     LimelightHelpers.setPipelineIndex("limelight-front", pipeline);
     piecesScored=0;
   }
@@ -96,6 +130,7 @@ public class targetFollow extends Command {
     
     switch(step){
       case DRIVE_OFF_START:
+      //not used
       driveSubsystem.drive(0.2, 0, 0, false);
       elevatorSubsystem.setTarget(0);
       if(System.currentTimeMillis()>timer+4000){
@@ -118,11 +153,12 @@ public class targetFollow extends Command {
         rot = 0;
       }
       // x (forward) speed calculation based on size of target in camera view
-      double xSpeed = (SCORE_DISTANCE-ta)/(SCORE_DISTANCE*5);//ta=SCORE_DISTANCE is the target (bigger is closer), 0.2 is the speed limit, xSpeed is forward/reverse
+      //tested working values (SCORE_DISTANCE-ta)/(SCORE_DISTANCE*5)
+      double xSpeed = (SCORE_DISTANCE-ta)/(SCORE_DISTANCE*2.5);//ta=SCORE_DISTANCE is the target (bigger is closer), 0.2 is the speed limit, xSpeed is forward/reverse
       if(xSpeed < XSPEED_DEADBAND && xSpeed>-XSPEED_DEADBAND){
         xSpeed = 0;
       }
-      if (xSpeed>0.15) {xSpeed=0.15;}//limit forward speed approaching target
+      if (xSpeed>MAX_FORWARD_SPD) {xSpeed=MAX_FORWARD_SPD;}//limit forward speed approaching target
       else if (xSpeed<-0.05) {xSpeed=-0.05;}//limit reverse speed adjusting back from target
       // y (lateral) speed calculation
       //double ySpeed = (-16-tx)/50;//- move right; center pixel based translation
@@ -133,9 +169,10 @@ public class targetFollow extends Command {
       else if (ySpeed<-0.05){ySpeed=-0.05;}//enforce lateral speed limit
       if (ta<SCORE_DISTANCE*0.5){ySpeed = 0;}//only move lateral if close to target
       //no target detected
-      if (!tv) {xSpeed = 0.05;rot = 0; ySpeed =0;}// what to do if no target is detected
+      //tested default speed 0.05
+      if (!tv) {xSpeed = 0.1;rot = 0; ySpeed =0;}// what to do if no target is detected
       driveSubsystem.drive(xSpeed, 0, rot, false);
-      if(Math.abs(X_OFFSET-tx)<1&&Math.abs(SCORE_DISTANCE-ta)<0.5){
+      if(Math.abs(X_OFFSET-tx)<1&&Math.abs(SCORE_DISTANCE-ta)<0.5||System.currentTimeMillis()>timer+DRIVE_TO_REEF_TIMEOUT){
         //&&Math.abs(targetpose[4])<8 //criteria for yaw control
         //check to switch steps
         step = FINAL_APPROACH;// switch to final movement
@@ -155,6 +192,12 @@ public class targetFollow extends Command {
         piecesScored++;
         rollerSubsystem.runRoller(0, 0);
         elevatorSubsystem.setTarget(0);
+        //for center scoring robot, stop at this point
+        if(position==1){
+//tagfollower position 0 - left, 1 - center, 2 - right
+step = 99;//no further steps
+        }
+
         System.out.println("score done");
         timer = System.currentTimeMillis();
         break;
@@ -162,7 +205,7 @@ public class targetFollow extends Command {
       break;
 
       case FINAL_APPROACH://final movement to reef
-      driveSubsystem.drive(0.1, 0, 0, false);
+      driveSubsystem.drive(0.15, 0, 0, false);
       //move elevator to score level
       if(LimelightHelpers.getCurrentPipelineIndex("limelight-front")==0){
         elevatorSubsystem.setTarget(55);
@@ -174,7 +217,7 @@ public class targetFollow extends Command {
 
       }
       
-      if(System.currentTimeMillis()>timer+3000){// 2000 ms forward drive time
+      if(System.currentTimeMillis()>timer+FINAL_APPROACH_DWELL){// 2000 ms forward drive time
         step = SCORE;// switch to scoring
         
         timer = System.currentTimeMillis();
@@ -184,9 +227,18 @@ public class targetFollow extends Command {
       case ROTATE_TO_LOAD:
       elevatorSubsystem.setTarget(0);
       if(piecesScored>1){
+        //subsequent scores back up directly without rotating
         driveSubsystem.drive(-0.1, 0, 0, false);
       }else{
-        driveSubsystem.drive(-0.1, 0, 0.3, false);
+        //need to determine left vs right rotation for first backup
+        if(position==0){
+          //left side back up counter-clockwise
+          driveSubsystem.drive(-0.1, 0, 0.3, false);
+        }else if (position == 2){
+          //right side back up clockwise
+          driveSubsystem.drive(-0.1, 0, -0.3, false);
+        }
+        
       }
       
       if(System.currentTimeMillis()>timer+1000){
@@ -207,8 +259,8 @@ public class targetFollow extends Command {
       xSpeed = (LOAD_DISTANCE-load_ta)/(LOAD_DISTANCE);//ta=SCORE_DISTANCE is the target (bigger is closer), 0.2 is the speed limit, xSpeed is forward/reverse
       SmartDashboard.putNumber("xspeed", xSpeed);
       SmartDashboard.putNumber("load_ta", load_ta);
-      if(xSpeed>0.2){
-        xSpeed=0.2;//set maximum speed moving toward load
+      if(xSpeed>MAX_FORWARD_SPD){
+        xSpeed=MAX_FORWARD_SPD;//set maximum speed moving toward load
       }else if (xSpeed<-0.2){
         xSpeed=-0.2;
       }
@@ -222,10 +274,10 @@ public class targetFollow extends Command {
       if (ta<LOAD_DISTANCE/2){ySpeed = 0;}//only move lateral if close to target
 
       //no target detected code
-      if (!load_tv) {xSpeed = 0.1;rot = 0; ySpeed =0;}// what to do if no target is detected, NECESSARY for initial move off wall
+      if (!load_tv) {xSpeed = 0.15;rot = 0; ySpeed =0;}// what to do if no target is detected, NECESSARY for initial move off wall
       
       driveSubsystem.drive(-xSpeed, 0, rot, false);
-      if(Math.abs(load_tx)<0.5&&Math.abs(LOAD_DISTANCE-load_ta)<0.5){//need validity check tv
+      if(Math.abs(load_tx)<0.5&&Math.abs(LOAD_DISTANCE-load_ta)<0.5||System.currentTimeMillis()>timer+MOVE_TO_LOAD_TIMEOUT){//need validity check tv
         //check to switch steps
         step = WAIT_AT_LOAD;// switch to final movement
         timer = System.currentTimeMillis();
@@ -238,7 +290,7 @@ public class targetFollow extends Command {
       elevatorSubsystem.setTarget(0);
       driveSubsystem.drive(-0.1,0,0, false);
       rollerSubsystem.runRoller(0, 0.2);
-      if(System.currentTimeMillis()>timer+3000){// 1000 ms forward drive time
+      if(System.currentTimeMillis()>timer+WAIT_AT_LOAD_DWELL){// 1000 ms forward drive time
         step = DRIVE_TO_REEF;// switch to scoring
         timer = System.currentTimeMillis();
         rollerSubsystem.runRoller(0, 0);
